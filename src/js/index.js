c2bd8e6a99a55035bdbb95cdd6c78c224492931f
ved <prakashh108@gmail.com>
2017-08-27 19:25:54 +0530

    Release candidate 2.

--- src/js/index.js
+++ src/js/index.js
@@ -2,112 +2,112 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Game extends React.Component {
-	constructor(){
-		super();
-		this.state = {
-			nextMovePosition:[2,2],
-			gameNumbers:this.initialData()
-		};
-		this.maxBound = this.state.gameNumbers.length-1; // boundary condition
-	    this.minBound = 0;								// boundary condition
-		this.isValidMoveIndex = false;
-	}
+    constructor() {
+        super();
+        this.state = {
+            nextMovePosition: [2, 2],
+            gameNumbers: this.initialData()
+        };
+        this.maxBound = this.state.gameNumbers.length - 1; // boundary condition
+        this.minBound = 0;								// boundary condition
+        this.isValidMoveIndex = false;
+    }
 
-	//Initail gameNumbers value
-	 initialData(){
-		return [
-			[2,1,6,11],
-			[9,13,5,8],
-			[4,3,"",15],
-			[10,14,12,7]
-		]
-	};
+    //Initail gameNumbers value
+    initialData() {
+        return [
+            [2, 1, 6, 11],
+            [9, 13, 5, 8],
+            [4, 3, "", 15],
+            [10, 14, 12, 7]
+        ]
+    };
 
-	//Get move positions for clicked element
-	getMovePositions(row,col) {
-		let  movePositions = [];
-		if(row-1 >= this.minBound){
-			movePositions.push([row-1,col]);
-		}
-		if((row+1 <= this.maxBound) || (row-1<this.minBound)){
-			movePositions.push([row+1,col]);
-		}
+    //Get move positions for clicked element
+    getMovePositions(row, col) {
+        let movePositions = [];
+        if (row - 1 >= this.minBound) {
+            movePositions.push([row - 1, col]);
+        }
+        if ((row + 1 <= this.maxBound) || (row - 1 < this.minBound)) {
+            movePositions.push([row + 1, col]);
+        }
 
-		if(col-1 >= this.minBound){
-			movePositions.push([row,col-1]);
-		}
+        if (col - 1 >= this.minBound) {
+            movePositions.push([row, col - 1]);
+        }
 
-		if((col+1 <= this.maxBound) || (col-1 < this.minBound)){
-			movePositions.push([row,col+1]);
-		}
-		return movePositions;
-	}
+        if ((col + 1 <= this.maxBound) || (col - 1 < this.minBound)) {
+            movePositions.push([row, col + 1]);
+        }
+        return movePositions;
+    }
 
-	//Reset game
-	 resetGame =()=>{
-		const nextMovePosition = [2,2];
-		this.setState({gameNumbers: this.initialData(), nextMovePosition: nextMovePosition})
-	};
+    //Reset game
+    resetGame = ()=> {
+        const nextMovePosition = [2, 2];
+        this.setState({gameNumbers: this.initialData(), nextMovePosition: nextMovePosition})
+    };
 
-	//get move positions and  Move clicked number to next movable index
-	calculateMove(rowNumber,colNumber,value) {
-		if(!value){
-			return;
-		}
-		let {nextMovePosition,gameNumbers} = this.state;
-		let movePositions = this.getMovePositions(rowNumber,colNumber);
+    //get move positions and  Move clicked number to next movable index
+    calculateMove(rowNumber, colNumber, value) {
+        if (!value) {
+            return;
+        }
+        let {nextMovePosition, gameNumbers} = this.state;
+        let movePositions = this.getMovePositions(rowNumber, colNumber);
 
-		movePositions.forEach((rowPosition,i)=>{
-				if(JSON.stringify(movePositions[i])==JSON.stringify(nextMovePosition) && !this.isValidMoveIndex){
-					this.isValidMoveIndex = true;
-				}
-		});
+        movePositions.forEach((rowPosition, i)=> {
+            if (JSON.stringify(movePositions[i]) == JSON.stringify(nextMovePosition) && !this.isValidMoveIndex) {
+                this.isValidMoveIndex = true;
+            }
+        });
 
-		if(this.isValidMoveIndex){
-			gameNumbers[nextMovePosition[0]][nextMovePosition[1]] = gameNumbers[rowNumber][colNumber];
-			gameNumbers[rowNumber][colNumber] = "";
-			nextMovePosition = [rowNumber,colNumber];
+        if (this.isValidMoveIndex) {
+            gameNumbers[nextMovePosition[0]][nextMovePosition[1]] = gameNumbers[rowNumber][colNumber];
+            gameNumbers[rowNumber][colNumber] = "";
+            nextMovePosition = [rowNumber, colNumber];
 
-			this.setState({gameNumbers:gameNumbers,nextMovePosition:nextMovePosition},()=>{
-				this.isValidMoveIndex = false;
-			})
-		}
-	}
+            this.setState({gameNumbers: gameNumbers, nextMovePosition: nextMovePosition}, ()=> {
+                this.isValidMoveIndex = false;
+            })
+        }
+    }
 
-	render() {
-		const {gameNumbers} =this.state;
+    render() {
+        const {gameNumbers} =this.state;
 
-		return (
-			<div id="game-layout">
-				<table>
-					<tbody>
-						<tr>
-							{
-								gameNumbers.map((row,i)=>{
-									return row.map((col,j)=>{
-										return (
-											<td onClick={()=>this.calculateMove(i,j,col)} key={i+j}>
-												<b>{col}</b>
-											</td>
-										)
-									})
-								})
-							}
-						</tr>
-					</tbody>
-				</table>
+        return (
+            <div id="game-layout">
+                <table>
+                    <tbody>
+                    <tr>
+                        {
+                            gameNumbers.map((row, i)=> {
+                                return row.map((col, j)=> {
+                                    return (
+                                        <td onClick={()=>this.calculateMove(i,j,col)} key={i+j}>
+                                            <b>{col}</b>
+                                        </td>
+                                    )
+                                })
+                            })
+                        }
+                    </tr>
+                    </tbody>
+                </table>
 
-				<button className=" repaly-btn btn btn-default" onClick={this.resetGame}>Start Again</button>
+                <button className=" repaly-btn btn btn-default" onClick={this.resetGame}>Start Again</button>
 
-			</div>
-		);
-	}
+            </div>
+        );
+    }
 }
 
 
 ReactDOM.render(
-	<Game/>,
-	document.getElementById('container')
+    <Game/>,
+    document.getElementById('container')
 );
 
 
